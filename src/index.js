e2c6b06a2b258d68ffbc9624478e8f5cb7095202
ismail.arilik <ismail.arilik@piworks.net>
2022-03-01 14:06:32 +0300

    Use single-spa to serve this app as a microfrontend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import angular from 'angular';
+import singleSpaAngularJS from 'single-spa-angularjs';
 import './hello-module';
+
+const ngLifecycles = singleSpaAngularJS({
+  angular: angular,
+  mainAngularModule: 'hello',
+  uiRouter: true,
+  preserveGlobal: false,
+  template: '<hello />',
+});
+
+export const bootstrap = ngLifecycles.bootstrap;
+export const mount = ngLifecycles.mount;
+export const unmount = ngLifecycles.unmount;
